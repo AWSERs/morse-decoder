9cfa9e21b62863a28892eebf17dea9ca66f786ee
Fedor Yakushev <fedor.yakushev92@mail.ru>
2022-02-05 21:34:04 +0300

    feat: morse-decoder done, test:  50/50

--- src/index.js
+++ src/index.js
@@ -37,8 +37,83 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const obj = {}
+Object.keys(MORSE_TABLE).forEach((value) => {
+    let key = MORSE_TABLE[value]
+    obj[key] = value
+})
+
 function decode(expr) {
-    // write your solution here
+  let arr = [...expr]
+  let tempArr = []
+  console.log(arr)
+  
+  for (let i = 0; i < arr.length; i++){
+    let temp;
+    if (i % 10 === 0) {
+      temp = 0
+      temp = arr[i]+arr[i+1]+arr[i+2]+arr[i+3]+arr[i+4]+arr[i+5]+arr[i+6]+arr[i+7]+arr[i+8]+arr[i+9]
+      tempArr.push(temp)
+    }
+  }
+
+  let morseArr = []
+  let temp = []
+  let morse = []
+  let count = 0
+  console.log(tempArr)
+  for (let i = 0; i < tempArr.length; i++){
+    let temp = [tempArr[i]]
+    temp.forEach(item => {
+      for (let i = 0; i < item.length; i++){
+        if (i % 2 === 0) {
+            morse.push(item[i] + item[i+1])
+          }
+      }
+    })
+  }
+  console.log(morse)
+  
+  for (i = 0; i <= morse.length; i++){
+    
+  
+    if (count === 5 && count !== 0) {
+      morseArr.push(temp)
+      temp = []
+      if (morse[i] === '11') {
+        temp.push('-')
+      } else if (morse[i] === '10') {
+        temp.push('.')
+      }
+      count = 1;
+      } else if (morse[i] === '11') {
+      temp.push('-')
+      count++
+      }else if(morse[i] === '10') {
+      temp.push('.')
+      count++
+    } else {
+      count++
+    }
+    
+  }
+  console.log(morseArr)
+  let result = []
+  morseArr.forEach(item => {
+    for (key in MORSE_TABLE) {
+      if (key === item.join('')) {
+        result.push(MORSE_TABLE[key])
+      } else if (item.length === 0) {
+        result.push(' ')
+      }
+    } 
+    
+  })
+  console.log(result)
+  
+  
+
+  return result.join('').replace(/\s+/g, ' ').trim()
 }
 
 module.exports = {
